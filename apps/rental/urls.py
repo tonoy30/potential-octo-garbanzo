89a917bae078cb8092e27f204d4a2c3a5693d230
tonoy30 <tonoy.sust@gmail.com>
2022-06-08 02:02:53 +0600

    url

--- apps/rental/urls.py
+++ apps/rental/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
          ProductRental.as_view({'post': 'calculate_product_rent'}), name='products-rent'),
     path('products/rent/confirmed', ProductRental.as_view({'post': 'confirmed_product_rent'}),
          name='products-rent-confirmed'),
-    path('products/rent/return', ProductRental.as_view({'post': 'return_product'}),
+    path('products/return', ProductRental.as_view({'post': 'return_product'}),
          name='products-rent-return'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
