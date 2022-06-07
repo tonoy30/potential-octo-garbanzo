fd01e141bb4e3f2634d723445f43b1966b30de4c
tonoy30 <tonoy.sust@gmail.com>
2022-06-07 20:52:15 +0600

    urls

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'whitenoise.runserver_nostatic',
-    'apps.rental'
+    'apps.rental',
 ]
 
 MIDDLEWARE = [
@@ -146,5 +146,5 @@ REST_FRAMEWORK = {
 # CORS
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3000"
 ]
